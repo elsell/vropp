0ed8ea3d72dfe50cff1d6a62dc10e734e93dba20
John <JSGreat@users.noreply.github.com>
2020-03-03 15:37:28 -0600

    Restructure some code, better 0 device behavior

--- volume.py
+++ volume.py
@@ -7,21 +7,23 @@ try:
 except ImportError:
     import pyreadline as readline
 
+
 hLine = "=" * 80
 header = ""
 about = "The One-Stop Shop for Annoying Your Fellow Google Cast Users"
 with open("header", "rb") as headFile:
     header =  headFile.read().decode("UTF-16")
 
-print(hLine)
-[print(line.center(80)) for line in header.splitlines()]
-print(about.center(80))
-print(hLine)
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-print("Performing Discovery for Cast Devices...")
-l = (pychromecast.get_chromecasts())
-print("Discovered {} Cast Devices:".format(len(l)))
-print()
+def printHeader():
+    cls()
+    print(hLine)
+    [print(line.center(80)) for line in header.splitlines()]
+    print(about.center(80))
+    print(hLine,"\n")
+
 
 def completer(text, state):
     options = [cmd for cmd in [cc.device.friendly_name for cc in l] if cmd.startswith(text)]
@@ -31,27 +33,46 @@ def completer(text, state):
         return None
 
 readline.parse_and_bind("tab: complete")
-readline.set_completer(completer)
+readline.set_completer(completer)    
+
+printHeader()
+print("Performing Discovery for Cast Devices...")
+l = (pychromecast.get_chromecasts())
+
+# It's pretty useless to run without any targets...so don't!
+if len(l) == 0:
+    print("[ fail ] No Cast Devices Found")
+    exit(0)
+
+print("Discovered {} Cast Devices:\n".format(len(l)))
+
 
 while(True):
-    print("TARGETS\n" + "-"*80)
+    # PRINT TARGET LIST
+    print("-"*80)
     [print(n) for n in sorted([cc.device.friendly_name for cc in l])]
-    print("_"*80,"\nEND TARGETS\n")
+    print("\n" + "_"*80)
+
+    # TARGET PROMPT 
     target = input("selectTarget> ")
     print("Connecting to {}...".format(target))
+
+    # ATTEMPT TO CONNECT
     try:
         cast = next(cc for cc in l if cc.device.friendly_name == target)
         cast.wait()
-        print("Connected.")
+        print("Connected to {}.".format(target))
         num = input("attackRange> ")
-        print("Attacking for range {}...".format(num))
+        print("Attacking with range {}...".format(num))
         for i in range(0, int(num)):
             i = i % 10
             i = i / 10
             cast.set_volume(i)
             sleep(.4)
         print("Attack Complete.\n\n")
-        input("[ENTER] To Continue...")
 
     except Exception as e:
         print("[ fail ] Could not connect to {}. Exception: {}".format(target,str(e)))
+
+    input("\n[ENTER] To Continue...")
+    printHeader()
